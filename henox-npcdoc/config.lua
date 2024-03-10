1cf7c86f7706b903b9df232b040cecf73564423e
henoxhnx <97401557+henoxhnx@users.noreply.github.com>
2024-03-10 05:16:04 +0300

    Add files via upload

---
+++ henox-npcdoc/config.lua
@@ -0,0 +1,9 @@
+-------------------
+-- CONFIG --
+-------------------
+Config = {}
+
+Config.Doctor = 0 -- Minimum Amount of EMS to work
+Config.Price = 10000
+Config.ReviveTime = 45000  --in msec
+
---
+++ henox-npcdoc/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'HENoX NPC DOC'
+
+version '0.1.0'
+
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+} 
